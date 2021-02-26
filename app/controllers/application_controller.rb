5b28e071c245026c9553edd0310ffb3b71a0ecc8
KeslieBox <kesliebox@gmail.com>
2021-02-26 16:46:23 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-binding.pry
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:user_id] = @user.id
@@ -46,6 +45,7 @@ binding.pry
   end
 
   get '/users/home' do
+    binding.pry
 
     @user = User.find(session[:user_id])
     erb :'/users/home'
